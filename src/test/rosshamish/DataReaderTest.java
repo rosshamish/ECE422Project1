e2669ce3bfe4a50e20156382dcc5895ff80c66eb
Ross Anderson <ross.anderson@ualberta.ca>
2016-02-11 01:51:24 -0700

    Implement DataReader

--- src/test/rosshamish/DataReaderTest.java
+++ src/test/rosshamish/DataReaderTest.java
@@ -1,14 +1,29 @@
 package test.rosshamish;
 
+import org.junit.After;
 import org.junit.Test;
 import rosshamish.DataGenerator;
 import rosshamish.DataReader;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class DataReaderTest {
+
+    @After
+    public void deleteTestFile() {
+        try {
+            if (Files.exists(Paths.get(DataGenerator.testPath))) {
+                Files.delete(Paths.get(DataGenerator.testPath));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void testRead() throws Exception {
--- src/test/rosshamish/DataSorterBackupTest.java
+++ src/test/rosshamish/DataSorterBackupTest.java
@@ -1,12 +1,6 @@
 package test.rosshamish;
 
 import org.junit.Test;
-import rosshamish.DataGenerator;
-import rosshamish.DataSorter;
-import rosshamish.DataSorterBackup;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.fail;
 
@@ -14,20 +8,22 @@ public class DataSorterBackupTest {
 
     @Test
     public void testSortData() throws Exception {
-        DataGenerator dg = new DataGenerator();
-        dg.generateData(DataGenerator.testPath, 100);
-
-        DataSorter sorter = new DataSorterBackup();
-        List<Integer> integers = new ArrayList<Integer>() {{
-            add(0);
-            add(1);
-            add(9);
-            add(-1);
-            add(0);
-            add(100);
-            add(-100);
-        }};
         fail("Test case not yet implemented");
+
+//        DataGenerator dg = new DataGenerator();
+//        dg.generateData(DataGenerator.testPath, 100);
+//
+//        DataSorter sorter = new DataSorterBackup();
+//        List<Integer> integers = new ArrayList<Integer>() {{
+//            add(0);
+//            add(1);
+//            add(9);
+//            add(-1);
+//            add(0);
+//            add(100);
+//            add(-100);
+//        }};
+
 //        List<Integer> sorted = sorter.sort(integers);
 //        Integer latest = sorted.get(0);
 //        for (Integer i: sorted) {
